c42e021727614312c34c5a24132ba3c691c1ee9a
fotomxq <fotomxq@gmail.com>
2013-03-12 12:23:48 +0800

    更新数据库设计
    *为所有ID和user、user_group、ip_addr，以及相关的外键约束字段为唯一。
    *修改一些字段为约束字段。
    *合并user_create_ip和user_login_ip字段。
    *修正posts表为InnoDB引擎。
    *修改user_create_date字段名称为user_date。
    *修复Posts表id主键。

--- content/install/install.sql
+++ content/install/install.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2013 年 03 月 11 日 07:27
+-- 生成日期: 2013 年 03 月 12 日 04:14
 -- 服务器版本: 5.1.44
 -- PHP 版本: 5.3.1
 
@@ -24,7 +24,9 @@ CREATE TABLE IF NOT EXISTS `core_ip` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `ip_addr` varchar(39) COLLATE utf8_bin NOT NULL COMMENT 'IP地址',
   `ip_ban` tinyint(1) NOT NULL COMMENT 'IP封锁状态',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `ip_addr` (`ip_addr`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
 
 --
@@ -44,9 +46,11 @@ INSERT INTO `core_ip` (`id`, `ip_addr`, `ip_ban`) VALUES
 CREATE TABLE IF NOT EXISTS `core_log` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `log_date` datetime NOT NULL COMMENT '创建时间',
-  `log_ip` bigint(20) NOT NULL COMMENT '宿主IP ID',
+  `log_ip` bigint(20) unsigned NOT NULL COMMENT '宿主IP ID',
   `log_message` text COLLATE utf8_bin NOT NULL COMMENT '描述消息',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `log_ip` (`log_ip`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -60,7 +64,9 @@ CREATE TABLE IF NOT EXISTS `oa_configs` (
   `config_name` varchar(300) COLLATE utf8_bin NOT NULL,
   `config_value` text COLLATE utf8_bin NOT NULL,
   `config_default` text COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `config_name` (`config_name`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=3 ;
 
 --
@@ -78,22 +84,28 @@ INSERT INTO `oa_configs` (`id`, `config_name`, `config_value`, `config_default`)
 --
 
 CREATE TABLE IF NOT EXISTS `oa_posts` (
-  `id` bigint(20) NOT NULL COMMENT '主键',
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
   `post_title` varchar(300) COLLATE utf8_bin DEFAULT NULL COMMENT '标题',
   `post_content` longtext COLLATE utf8_bin COMMENT '内容',
   `post_date` datetime NOT NULL COMMENT '创建时间',
   `post_modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
-  `post_ip` bigint(20) NOT NULL COMMENT 'IP ID',
+  `post_ip` bigint(20) unsigned DEFAULT NULL COMMENT 'IP ID',
   `post_type` varchar(300) COLLATE utf8_bin NOT NULL COMMENT '内容类型标识',
   `post_order` int(11) NOT NULL DEFAULT '0' COMMENT '排序',
-  `post_parent` bigint(20) NOT NULL COMMENT '上一级ID',
-  `post_user` bigint(20) DEFAULT NULL COMMENT '用户ID',
+  `post_parent` bigint(20) unsigned NOT NULL COMMENT '上一级ID',
+  `post_user` bigint(20) unsigned DEFAULT NULL COMMENT '用户ID',
   `post_password` char(41) COLLATE utf8_bin NOT NULL COMMENT '访问密码',
   `post_name` varchar(300) COLLATE utf8_bin DEFAULT NULL COMMENT '媒体文件名称',
   `post_url` varchar(500) COLLATE utf8_bin DEFAULT NULL COMMENT '多媒体文件路径',
   `post_status` varchar(30) COLLATE utf8_bin NOT NULL DEFAULT 'public' COMMENT '发布状态',
-  `post_meta` varchar(300) COLLATE utf8_bin DEFAULT NULL COMMENT '头信息'
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  `post_meta` varchar(300) COLLATE utf8_bin DEFAULT NULL COMMENT '头信息',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_2` (`id`),
+  KEY `post_ip` (`post_ip`),
+  KEY `post_parent` (`post_parent`),
+  KEY `post_user` (`post_user`),
+  KEY `id` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -107,15 +119,19 @@ CREATE TABLE IF NOT EXISTS `oa_user` (
   `user_password` char(41) COLLATE utf8_bin NOT NULL COMMENT '密码',
   `user_email` varchar(255) COLLATE utf8_bin NOT NULL COMMENT '邮箱',
   `user_name` varchar(30) COLLATE utf8_bin NOT NULL COMMENT '匿名',
-  `user_group` bigint(20) NOT NULL COMMENT '用户组',
-  `user_create_date` datetime NOT NULL COMMENT '创建时间',
-  `user_create_ip` bigint(20) NOT NULL COMMENT '创建IP ID',
+  `user_group` bigint(20) unsigned NOT NULL COMMENT '用户组',
+  `user_date` datetime NOT NULL COMMENT '创建时间',
   `user_login_date` datetime NOT NULL COMMENT '上一次登录时间',
-  `user_login_ip` bigint(20) NOT NULL COMMENT '上一次登录IP ID',
+  `user_ip` bigint(20) unsigned NOT NULL COMMENT '登录IP ID',
   `user_login_session` char(32) COLLATE utf8_bin NOT NULL COMMENT '登录会话值',
   `user_status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '状态',
   `user_remember` tinyint(1) NOT NULL DEFAULT '0' COMMENT '记住我',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `user_username` (`user_username`),
+  KEY `user_ip` (`user_ip`),
+  KEY `user_ip_2` (`user_ip`),
+  KEY `user_group` (`user_group`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -129,5 +145,32 @@ CREATE TABLE IF NOT EXISTS `oa_user_group` (
   `group_name` varchar(30) COLLATE utf8_bin NOT NULL COMMENT '名称',
   `group_power` text COLLATE utf8_bin NOT NULL COMMENT '权限',
   `group_status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '状态',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `group_name` (`group_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+
+--
+-- 限制导出的表
+--
+
+--
+-- 限制表 `core_log`
+--
+ALTER TABLE `core_log`
+  ADD CONSTRAINT `core_log_ibfk_2` FOREIGN KEY (`log_ip`) REFERENCES `core_ip` (`id`) ON UPDATE CASCADE;
+
+--
+-- 限制表 `oa_posts`
+--
+ALTER TABLE `oa_posts`
+  ADD CONSTRAINT `oa_posts_ibfk_3` FOREIGN KEY (`post_ip`) REFERENCES `core_ip` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `oa_posts_ibfk_1` FOREIGN KEY (`post_parent`) REFERENCES `oa_posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `oa_posts_ibfk_2` FOREIGN KEY (`post_user`) REFERENCES `oa_user` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- 限制表 `oa_user`
+--
+ALTER TABLE `oa_user`
+  ADD CONSTRAINT `oa_user_ibfk_3` FOREIGN KEY (`user_group`) REFERENCES `oa_user` (`user_group`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `oa_user_ibfk_4` FOREIGN KEY (`user_ip`) REFERENCES `core_ip` (`id`) ON UPDATE CASCADE;
